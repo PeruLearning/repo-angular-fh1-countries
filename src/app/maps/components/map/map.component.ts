0019b5f83d4a904c8631be2f56a649be44aa5fbc
Edinson Aldaz <edinson.aldaz@gmail.com>
2024-06-26 06:36:23 -0500

    Ajustes con loading

--- src/app/maps/components/map/map.component.ts
+++ src/app/maps/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { LatLong } from '../../interfaces/lat-long.interface';
 import * as leaflet from 'leaflet';
 
@@ -19,19 +19,18 @@ export class MapComponent implements AfterViewInit {
   @Input()
   public originalPoint: LatLong = { latitude: 0, longitude: 0 };
 
-  constructor() {
+  constructor(private cd: ChangeDetectorRef) {
     this.mapId = this.generateGuid();
   }
-  ngAfterViewInit(): void {
+
+  public ngAfterViewInit(): void {
     this.createMap(this.originalPoint, this.zoomLevel);
   }
 
-  //@Output()
-  // public onCreatedMap: EventEmitter<MapComponent> = new EventEmitter();
-
   private createMap(point: LatLong, initialZoomLevel: number, maxZoomLevel: number = 19) {
 
-    let map = leaflet.map(this.mapId).setView([point.latitude, point.longitude], initialZoomLevel);
+    let map = leaflet.map(this.mapId)
+      .setView([point.latitude, point.longitude], initialZoomLevel);
 
     leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: maxZoomLevel,
@@ -39,6 +38,8 @@ export class MapComponent implements AfterViewInit {
     }).addTo(map);
 
     console.log(`map ${this.mapId} created.`);
+    this.isMapReady = true;
+    this.cd.detectChanges();
   }
 
   private generateGuid(): string {
